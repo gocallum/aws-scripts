b8821bf0b5bbecbebacc8ecd6618d884f6d455ff
gocallum <callumbir@gmail.com>
2023-09-17 14:11:27 +1000

    added the .zsh file for MacOS for latest versions. Changed the files so that the instance names you see in EC2 are coming from a variable at the beginning of file so that you change this easily.

--- create-ec2.zsh
+++ create-ec2.zsh
@@ -1,23 +1,38 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
 # Ensure AWS CLI is installed and configured
 
 # Define parameters
-REGION=us-east-1
-INSTANCE_TYPE=t2.micro
-AMI_ID=ami-08a52ddb321b32a8c
-KEY_NAME=mybootcamp-key
-KEY_FILE_PATH="./${KEY_NAME}.pem"
-SECURITY_GROUP_NAME=NodeJsSecurityGroup
-INSTANCE_NAME=mybootcamp-instance   # Here is your variable for instance name
 
-# Check if the key pair already exists
+# Region where the instance will be launched.
+REGION=us-east-1
+
+# The EC2 instance type determines the hardware of the host computer used for the instance.
+# 't2.micro' is one of the instance types eligible for the AWS Free Tier and is suitable for low traffic applications.
+INSTANCE_TYPE=t2.micro
+
+# The Amazon Machine Image (AMI) provides the information required to launch an instance.
+# This specific AMI ID represents an AWS Linux instance. Ensure this AMI is available in the specified region or update the ID.
+AMI_ID=ami-08a52ddb321b32a8c
+
+# The name for the key pair used for connecting to the EC2 instance.
+# WARNING: Secure the key (.pem file). If lost or shared, anyone with the key can access your EC2 instance.
+KEY_NAME=mybootcamp-key
+KEY_FILE_PATH="$PWD/$KEY_NAME.pem"
+
+# Security Group that will define which incoming/outgoing traffic is allowed to/from the instance.
+SECURITY_GROUP_NAME=NodeJsSecurityGroup
+INSTANCE_NAME=mybootcamp-instance
+
+# Create the Key Pair only if it doesn't exist
+echo "Checking for key pair..."
 EXISTING_KEY_NAME=$(aws ec2 describe-key-pairs --region $REGION --query "KeyPairs[?KeyName=='$KEY_NAME'].KeyName | [0]" --output text)
 
-if [ "$EXISTING_KEY_NAME" != "$KEY_NAME" ]; then
+if [[ "$EXISTING_KEY_NAME" != "$KEY_NAME" ]]; then
     echo "Creating key pair..."
     aws ec2 create-key-pair --key-name $KEY_NAME --region $REGION --query "KeyMaterial" --output text > $KEY_FILE_PATH
     echo "Key pair saved to $KEY_FILE_PATH"
+    # IMPORTANT: This key (.pem file) will be used to connect to the EC2 instance via SSH. Store it securely and do not share.
 else
     echo "Key pair $KEY_NAME already exists."
 fi
@@ -26,10 +41,11 @@ fi
 SG_ID=$(aws ec2 describe-security-groups --region $REGION --query "SecurityGroups[?GroupName=='$SECURITY_GROUP_NAME'].GroupId | [0]" --output text)
 
 # If security group doesn't exist, create it and add rules
-if [ -z "$SG_ID" ]; then
+if [[ -z "$SG_ID" ]]; then
     SG_ID=$(aws ec2 create-security-group --region $REGION --group-name $SECURITY_GROUP_NAME --description "SG for EC2 instance to run Node.js" --query "GroupId" --output text)
-    # Add rules
+    # Add SSH rule - Allows SSH access from any IP. WARNING: This is insecure for production environments.
     aws ec2 authorize-security-group-ingress --region $REGION --group-id $SG_ID --protocol tcp --port 22 --cidr 0.0.0.0/0
+    # Add rule for port 3000 - Allows incoming traffic on port 3000 from any IP. This is commonly used for web apps.
     aws ec2 authorize-security-group-ingress --region $REGION --group-id $SG_ID --protocol tcp --port 3000 --cidr 0.0.0.0/0
 fi
 
